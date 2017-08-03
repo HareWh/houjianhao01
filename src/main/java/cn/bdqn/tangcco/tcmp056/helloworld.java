0fc19fb36f4fb50e3ac1c755557b8c56d409567f
houjianhao <934163168@qq.com>
2017-08-03 19:02:09 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/helloworld.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by MACHENIKE on 2017/8/3.
+ */
+public class helloworld {
+    @Test
+    public void Hello() {
+        System.out.println("hello world");
+    }
+
+}
